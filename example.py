98426f33d42e4f69e6e068aa7c5d83e017374275
Bentsi Magidovich <bentsi@scylladb.com>
2020-10-08 19:17:47 +0300

    improvement(example): added decryption example code

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
 from caesarcipher.encrypt import encrypt
+from caesarcipher.decrypt import decrypt
 
 
 if __name__ == '__main__':
-    print(encrypt("verycool", step=10))
+    cipher_step = 10
+    encrypted_message = encrypt(text="verycool", step=cipher_step)
+    print(f"Encrypted text: {encrypted_message}")
+    decrypted_text = decrypt(encrypted_text=encrypted_message, step=cipher_step)
+    print(f"Decrypted text: {decrypted_text}")
